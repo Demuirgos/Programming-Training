4cb5cb0b03fe54f9519c2a12cedb6586333b42a1
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-12-02 20:42:24 +0100

    Day 2

--- Advent of Code/2021/Day2.fsx
+++ Advent of Code/2021/Day2.fsx
@@ -1,11 +1,13 @@
 open System.IO
 type Instruction = Up of int | Down of int | Forward of int | NoMove
+type Part = Part1 | Part2
 type State = {
     Horizontal : int
+    Aim : int
     Depth : int
 }
-let init = {Horizontal = 0; Depth = 0}
-let instructions =  File.ReadLines(@"./Input/Day2.txt")
+let init = {Horizontal = 0; Depth = 0; Aim = 0}
+let instructions =  File.ReadLines("Day2.txt")
                     |> Seq.map (fun e -> e.Trim().Split ' ')
                     |> Seq.map (fun e -> match e.[0] with 
                                     | "up" -> Up(int e.[1])
@@ -13,16 +15,27 @@ let instructions =  File.ReadLines(@"./Input/Day2.txt")
                                     | "down" -> Down(int e.[1])
                                     | _ -> NoMove)
                     |> Seq.toList
-let rec apply state = 
+let rec apply state mode =
     function 
     | [] -> state
     | h::t -> 
-        match h with 
-        | Up(v) -> apply { state with Depth = state.Depth - v } t
-        | Down(v) -> apply { state with Depth = state.Depth + v } t
-        | Forward(v) -> apply { state with Horizontal = state.Horizontal + v } t
-        | NoMove -> state
+    match mode with 
+        | Part1 -> 
+            match h with 
+            | Up(v) -> apply { state with Depth = state.Depth - v } mode t 
+            | Down(v) -> apply { state with Depth = state.Depth + v } mode t 
+            | Forward(v) -> apply { state with  Horizontal = state.Horizontal + v } mode t
+            | NoMove -> state
+        | Part2 -> 
+            match h with
+            | Up(v) -> apply { state with Aim = state.Aim - v } mode t
+            | Down(v) -> apply { state with Aim = state.Aim + v } mode t
+            | Forward(v) -> apply { state with  Horizontal = state.Horizontal + v 
+                                                Depth = state.Depth + state.Aim * v} mode t
+            | NoMove -> state
 
-let part1 = 
-    let result = apply init instructions
-    result.Depth * result.Horizontal 
+let results = 
+    let result1 = apply init Part1 instructions
+    let result2 = apply init Part2 instructions
+    {|Part1 = result1.Depth * result1.Horizontal ; 
+      Part2 = result2.Depth * result2.Horizontal|}
