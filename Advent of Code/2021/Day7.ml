0e91f5788e2c36a982dfe5ac36a32f6ec79d9031
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-12-07 23:41:13 +0100

    Day 7

--- Advent of Code/2021/Day7.ml
+++ Advent of Code/2021/Day7.ml
@@ -1,9 +1,32 @@
+open Core.Std
+open Prevasives
+type mode = 
+    Part1 
+    | Part2
 let sum l = List.fold_right (+) l 0 
 let median l = 
   let lenght = List.length l in 
   lenght / 2
+let mean l = 
+  let add = sum l in
+  let len = List.length l in 
+  add / len 
 let abs i = if i < 0 then -i else i
-let solve data = 
+let consumn = 
+  let rec loop acc n = 
+    if n == 0 then acc else loop (acc + n) (n - 1) in 
+  loop 0
+let solve mode =
+  let data = List.map int_of_string (In_channel.read_lines ".\Input\Day7.txt") in
   let input = List.sort (fun i j -> i - j) data in
-  let medin = List.nth input (median input) in 
-  sum (List.map (fun i -> abs (i - medin)) input)
+  match mode with
+      Part1 -> 
+    	let medin = List.nth sorted (median input) in 
+    	let result = sum (List.map (fun i -> abs (i - medin)) input) in
+    	result
+      | Part2 ->
+    	let mean = mean input in 
+    	let getFuel v = sum (List.map (fun i -> consumn (abs (i - v))) input) in
+    	let pot1, pot2 = getFuel mean, getFuel (mean + 1) in
+    	let result = if pot1 > pot2 then pot2 else pot1 in
+    	result
