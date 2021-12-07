cb44bd4c431a4d5bf76f970ea0cbaca8e5aa8ef3
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-12-07 19:36:43 +0100

    Update Day6.cs
    attempt 2

--- Advent of Code/2021/Day6.cs
+++ Advent of Code/2021/Day6.cs
@@ -2,11 +2,10 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-List<int> Yield(List<int> l, params int[] i) { l.AddRange(i); return l; }
 
-var input = File.ReadAllLines(@".\Input\Day6.txt").SelectMany(l => l.Split(',').Select(Int32.Parse).ToList()).ToList();
-var Next = (List<int> previous) => previous.Aggregate(new List<int>(), (acc, item) => item != 0 ? Yield(acc, item -1): Yield(acc, 6, 8));
-List<int> Engine(List<int> state, int idx) => idx == 0 ? state : Engine(Next(state), idx - 1);
-
-var part1 = Engine(input, 80);
-var part2 = Engine(part1, 256-80);
+var Solve = (int d) => File.ReadAllLines(@".\Input\Day6.txt")
+  .SelectMany(l => l.Split(',').Select(Int32.Parse).ToList())
+  .GroupBy(i => i)
+	.Select(g => g.Count() * (1 + f(g.Key, d)))
+	.Sum();
+var (part1, part2) = (Solve(80), Solve(256));
