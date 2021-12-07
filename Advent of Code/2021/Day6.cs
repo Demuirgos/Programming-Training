f394b9540b8bd1a10d6ffbe9a404ceb94e4fdf5b
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-12-08 00:08:33 +0100

    Day 6

--- Advent of Code/2021/Day6.cs
+++ Advent of Code/2021/Day6.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
+long f(int n, int d) => (d - n - 0 > 0 ? 1 + f(6 , d - n  - 1) + f(8 , d - n  - 1) : 0) ;
+
 var Solve = (int d) => File.ReadAllLines(@".\Input\Day6.txt")
   .SelectMany(l => l.Split(',').Select(Int32.Parse).ToList())
   .GroupBy(i => i)
