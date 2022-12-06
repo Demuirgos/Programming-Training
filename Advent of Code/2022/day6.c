aab6f755ec46ea1aa04f748faf5459c79fa0c35a
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-12-06 22:35:17 +0100

    Update day6.c

--- Advent of Code/2022/day6.c
+++ Advent of Code/2022/day6.c
@@ -6,23 +6,21 @@ int main(int argc, char const *argv[])
     FILE* file = fopen("day6.txt", "r");
     int part = 2;
     if (file == NULL) {
-        printf("Error opening file");
+        return 1;
     }
     int alphabet[26] = {0};
     int start = 0, index = 1;
     int count = part == 2 ? 14 : 4;
     while(!feof(file)) {
         char c = fgetc(file);
-        if(alphabet[c - 'a'] == start) {
-            alphabet[c - 'a'] = index;
-        } else {
+        if(alphabet[c - 'a'] != start) {
             start = max(start, alphabet[c - 'a']);
-            alphabet[c - 'a'] = index;
         }
 
+        alphabet[c - 'a'] = index;
         if (index - start == count) {
             printf("%d", index);
-            break;
+            return 0;
         }
         index++;
     }
